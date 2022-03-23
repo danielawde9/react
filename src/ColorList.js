e858d94263db6b205d56a4f54dc7e708443d7f98
Daniel Awde <danielawde9@gmail.com>
2022-03-23 15:38:03 +0200

    new color

--- src/ColorList.js
+++ src/ColorList.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import Color from './Color'
 
-export default function ColorList({ color = []}) {
+export default function ColorList({ colors = []}) {
   return (
-    <div>ColorList</div>
+    <div>{
+      // map is function that accept a function as an agrument that accept up to 3 arguments, the map calls it each item for the array
+      // calls a defimed a callback function on element of an array, and returns an arry that contains the reuls 
+
+      // create a funtion that accept color as an argument
+      colors.map(color => <Color key={color.id} {...color}/>)
+      }
+      </div>
   )
 }
